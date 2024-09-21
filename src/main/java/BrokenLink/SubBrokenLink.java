4763c9c83477572c32604d2c5c946203af33008d
Sakthi Madhu <151857561+sakthimadhu07@users.noreply.github.com>
2024-09-21 07:36:38 +0530

    Update SubBrokenLink.java

--- src/main/java/BrokenLink/SubBrokenLink.java
+++ src/main/java/BrokenLink/SubBrokenLink.java
@@ -4,5 +4,7 @@ public class SubBrokenLink {
 	
 	public static void subBrokenLink() {
 		System.out.println("Broken Links Updated");
+		System.out.println("Broken Links subBrokenLink");
+		System.out.println("Broken Links subBrokenLink");
 	}
 }
